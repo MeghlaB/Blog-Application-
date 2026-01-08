11e5c05f4aec7a3799dc197d1061f36fb66660b8
MeghlaB <meghlab964@gmail.com>
2026-01-08 14:32:18 +0600

    code added

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,15 +1,22 @@
 import e, { Request, Response } from "express";
 import { postServices } from "./post.services";
 
-const createPost = async(req:Request,res:Response)=>{
-    try{
- const result =  await postServices.createPost(req.body)
- res.status(201).json(result)
-    }catch(error){
-       res.status(400).json({
-        error:'Post creation failed',
-        details:e
-       })
+const createPost = async (req: Request, res: Response) => {
+    try {
+        const user = req.user
+        if (!user) {
+            return res.status(400).json({
+                error: 'Unauthorized',
+
+            })
+        }
+        const result = await postServices.createPost(req.body, user.id as string)
+        res.status(201).json(result)
+    } catch (error) {
+        res.status(400).json({
+            error: 'Post creation failed',
+            details: e
+        })
     }
 }
 
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,7 +1,9 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { postController } from './post.controller'
+import auth, { UserRole } from '../../middleware/auth'
 const router = express.Router()
 
-router.post("/",postController.createPost)
+
+router.post("/", auth(UserRole.USER), postController.createPost)
 
 export const postRouter = router
